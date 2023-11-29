b71f917cf4703442d1d66bb1b6977408a15c1c06
Mistralys <s.mordziol@gmail.com>
2023-11-29 18:07:33 +0100

    Test suites,

---
+++ tests/bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* Main bootstrapper used to set up the testsuites environment.
+*
+* @package Application Utils
+* @subpackage Tests
+* @author Sebastian Mordziol <s.mordziol@mistralys.eu>
+*/
+
+declare(strict_types=1);
+
+const TESTS_ROOT = __DIR__;
+const APP_UTILS_TESTSUITE = 'true';
+
+$autoloader = __DIR__ . '/../vendor/autoload.php';
+
+if(!file_exists($autoloader))
+{
+    die('ERROR: The autoloader is not present. Please run composer install first.');
+}
+
+require_once $autoloader;
---
+++ tests/phpstan/constants.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * File containing constants so PHPStan can discover them.
+ *
+ * @package Application Utils
+ * @subpackage PHPStan
+ */
+
+declare(strict_types=1);
